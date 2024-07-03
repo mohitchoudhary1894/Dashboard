360046191bb8cc39c9be1fd8787e2ff96d7c0327
mohitchoudhary1894 <113041859+mohitchoudhary1894@users.noreply.github.com>
2024-07-03 20:23:22 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -86,3 +86,71 @@ with col2:
       fig = px.pie(filtered_df, values='Sales', names='Region', hole=0.5)
       fig.update_traces(text=filtered_df["Region"], textposition ="outside")
       st.plotly_chart(fig,use_container_width=True)
+      cl1,cl2=st.columns((2))
+      with cl1:
+           with st.expander("Category_ViewData"):
+            st.write(category_df.style.background_gradient(cmap = "Blues"))
+            csv= category_df.to_csv(index=False).encode('utf-8')
+            st.download_button("Download Data", data =csv, file_name="category.csv",mime= "text/csv",
+                            help ='Click here to download the data as a CSV file')
+      with cl2:
+           with st.expander("Region_ViewData"):
+            region = filtered_df.groupby(by = "Region", as_index = False)["Sales"].sum()
+            st.write(region.style.background_gradient(cmap = "Oranges"))
+            csv= region.to_csv(index=False).encode('utf-8')
+            st.download_button("Download Data", data =csv, file_name="Region.csv",mime= "text/csv",
+                            help ='Click here to download the data as a CSV file')
+filtered_df["month_year"] = filtered_df["Order Date"].dt.to_period("M")
+st.subheader('Time Series Analysis')
+
+linechar = pd.DataFrame(filtered_df.groupby(filtered_df["month_year"].dt.strftime("%Y : %b"))["Sales"].sum()).reset_index()
+fig2 = px.line(linechar, x = "month_year", y = "Sales", labels = {"Sales" : "Amount"}, height=500, width= 1000, template="gridon")
+st.plotly_chart(fig2,use_container_width=True)
+
+with st.expander("View Date of TimeSderies:"):
+     st.write(linechar.T.style.background_gradient(cmap="Blues"))
+     csv = linechar.to_csv(index=False).encode("utf-8")
+     
+     st.download_button('Download Data', data=csv, file_name="TimeSeries.csv", mime='text/csv')
+
+#Create a treem based on Region, category, sub-category
+st.subheader("Hierarchical view of sales using TreeMap")
+fig3 = px.treemap(filtered_df, path=["Region","Category","Sub-Category"],values="Sales",hover_data=["Sales"],color = "Sub-Category")
+fig3.update_layout(width = 800, height = 650)
+st.plotly_chart(fig3, use_container_width=True)
+chart1,chart2 = st.columns((2))
+with chart1:
+     st.subheader("Segment wise sales")
+     fig = px.pie(filtered_df , values= "Sales", names="Segment", template="plotly_dark")
+     fig.update_traces(text = filtered_df["Segment"], textposition ="inside")
+     st.plotly_chart(fig,use_container_width = True)
+with chart2:
+     st.subheader("Category wise sales")
+     fig = px.pie(filtered_df , values= "Sales", names="Category", template="gridon")
+     fig.update_traces(text = filtered_df["Category"], textposition ="inside")
+     st.plotly_chart(fig,use_container_width = True)
+
+import plotly.figure_factory as ff
+st.subheader(":point_right: Month wise Sub-Category Sales Summary")
+with st.expander("Summary_Table"):
+     df_sample = df[0:5][["Region", "State","City","Category","Sales","Profit","Quantity"]]
+     fig = ff.create_table(df_sample, colorscale = "Cividis")
+     st.plotly_chart(fig, use_container_width=True)
+
+     st.markdown("Month wise sub - chategory table")
+     filtered_df["month"] = filtered_df["Order Date"].dt.month_name()
+     sub_category_Year = pd.pivot_table(data = filtered_df, values="Sales", index = ["Sub-Category"],columns ="month")
+     st.write(sub_category_Year.style.background_gradient(cmap="Blues"))
+
+     #Creat a scatter plot
+     data1 = px.scatter(filtered_df, x= "Sales" , y = "Profit", size="Quantity")
+     data1['layout'].update(title = "Relationship between Sales and Profits using Scatter Plot.",
+                            titlefont = dict(size =20),xaxis = dict(title = "Sales",titlefont = dict(size=19)))
+
+     st.plotly_chart(data1,use_container_width=True)
+     
+     with st.expander("View Data"):
+          st.write(filtered_df.iloc[:50,1:20:2].style.background_gradient(cmap = "Oranges"))
+     #Download orignal DataSet
+     csv = df.to_csv(index=False).encode('utf-8')
+     st.download_button("Download Data",data = csv,file_name ="Data.csv", mime ="text/csv")
